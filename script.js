407262f7ee5b71432ec2dab89adbe70dfff1dc7a
utopiaemse <utopiaemse@gmail.com>
2024-08-17 14:11:16 +0330

    add js file

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 'use strict';
+
+//Selecting Element
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Starting Condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1.Generation a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2.Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `./images/dice-${dice}.png`;
+
+  // 3.Check for rolled 1 : if true,
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    //Add dice to current score
+  } else {
+    //switch to next player
+    switchPlayer();
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  // 1.Add current score to active player's score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  // 2.Check if player's score >=100
+  // Finish the game
+  //Switch to the next player
+  switchPlayer();
+});
