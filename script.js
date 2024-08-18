5dd087877e1b105d2d1597f696014cb6a6403dc6
utopiaemse <utopiaemse@gmail.com>
2024-08-18 21:43:37 +0330

    code completed😎

--- script.js
+++ script.js
@@ -13,13 +13,26 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //Starting Condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
@@ -59,9 +72,10 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2.Check if player's score >=100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
+      diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
@@ -69,9 +83,17 @@ btnHold.addEventListener('click', function () {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
+      btnHold.classList.add('disable--element');
+      btnRoll.classList.add('disable--element');
+      if (activePlayer === 0) {
+        btnRoll.textContent = 'Winner! Player 1 🎖️';
+      } else if (activePlayer === 1) {
+        btnRoll.textContent = 'Winner! Player 2 🎖️';
+      }
     } else {
       switchPlayer();
     }
   }
   //Switch to the next player
 });
+btnNew.addEventListener('click', init);
