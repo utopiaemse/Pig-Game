53fb6a85dc6bb815d0344ba91037e739211248b9
utopiaemse <utopiaemse@gmail.com>
2024-08-19 12:46:10 +0330

    fix bug

--- script.js
+++ script.js
@@ -31,12 +31,16 @@ const init = function () {
   player1El.classList.remove('player--winner');
   player0El.classList.add('player--active');
   player1El.classList.remove('player--active');
+  btnHold.classList.remove('disable--element');
+  btnRoll.classList.remove('disable--element');
+  btnRoll.textContent = '🎲 Roll dice';
 };
 init();
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
   currentScore = 0;
+
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
@@ -72,7 +76,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2.Check if player's score >=100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 20) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
